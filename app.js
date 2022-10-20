86ae21327869986de6725afb5ea401e8e7dabda3
Yasmin Santana <94482549+santyasm@users.noreply.github.com>
2022-10-20 20:00:25 -0300

    bootstrap and handlebars

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const { engine } = require('express-handlebars');
 
+//Congigurações
+//Template Engine
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+//Rotas
 app.get('/', (req, res) => {
-	res.send('test');
+	res.render('index');
 });
 
-const port = 5555;
+const port = 9898;
 app.listen(port);
